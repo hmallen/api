65c226dfbf9779413e52b046c4f041f502206e4c
hmallen <allenhm@gmail.com>
2018-05-30 16:13:19 -0500

    Fixed boolean variable name.

--- coinigy/coinigy.py
+++ coinigy/coinigy.py
@@ -55,7 +55,7 @@ class CoinigyREST:
 
                 return
 
-            if json:
+            if json_output:
                 return r.json()
 
             return pd.DataFrame(r.json()['data'])
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='coinigy',
-    version='0.1a.dev7',
+    version='0.1a.dev8',
     author='Hunter M. Allen',
     author_email='allenhm@gmail.com',
     license='TheUnlicence',
